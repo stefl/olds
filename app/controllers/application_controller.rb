9e8edcff5338a7d9926e4c6d6b48577dda59916e
Stef Lewandowski <stef@stef.io>
2015-04-13 18:27:01 +0100

    ordinalize

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+require 'active_support/core_ext/integer/inflections'
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
