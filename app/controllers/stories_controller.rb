eacc766f78619a0e1b12ebb53ebecbc69dbbe7fc
Stef Lewandowski <stef@stef.io>
2015-04-13 14:24:32 +0100

    get rid of dupe image

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,6 +1,6 @@
 class StoriesController < ApplicationController
 
-  
+
 
   def show
     @story = Story.friendly.find(params[:id])
@@ -9,6 +9,19 @@ class StoriesController < ApplicationController
     doc = Nokogiri::HTML(@story.body).css("#mw-content-text")
     doc.css(".hatnote").first.remove rescue nil
     doc.css(".mw-editsection, .toc, .infobox, .navbox, .metadata, .noprint, table.vertical-navbox, table.mbox-small").each {|d| d.remove }
+    if(@story.image.present?)
+      img_file_name = @story.image.split("/").last.sub(/^[0-9]+px/,"")
+      Rails.logger.info img_file_name
+      doc.css(".thumb").each do |thumb|
+        doc.css("img").each do |img|
+          img_candidate_file_name = img.attr("src").split("/").last.sub(/^[0-9]+px/,"")
+          Rails.logger.info img_candidate_file_name
+          if img_candidate_file_name == img_file_name
+            thumb.remove
+          end
+        end
+      end
+    end
     @story_body = doc.to_s.gsub("/wiki/", "http://en.wikipedia.org/wiki/")
   end
 
